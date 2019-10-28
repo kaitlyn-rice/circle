d0aa250f85c9ccc3849a45bbbe9ecc07a89546fd
Qiang Hao <qiang.hao@wwu.edu>
2019-10-28 10:10:43 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# cs145-lab4
+
+My Name: Please edit this.
+
+About This Project: Please edit this.
---
+++ src/main/java/Circle.java
@@ -0,0 +1,137 @@
+import java.util.*;
+
+//*******************************************************
+// Circle.java
+//
+// 
+//*******************************************************
+public class Circle {
+    // add your instances here
+
+    // constructors
+    // default constructor - to be modified
+    public Circle() {}
+
+    // customized constructor - to be modified
+    public Circle(double xV, double yV, double radiusV) {
+        
+    }
+    
+    //--------------------------------------------------------
+    // toString - return a String representation of
+    //            this circle in the following format:
+    //            center:(x,y)
+    //            radius: r
+    //--------------------------------------------------------
+    public String toString() {
+        // you will remove this line
+        return "";
+    }
+
+    //----------------------------------------------
+    // getX - returns the value of x
+    //----------------------------------------------
+    public double getX() {
+        // you will remove this line
+        return 0.0;
+
+    }
+    
+
+    //----------------------------------------------
+    // getY - returns the value of y
+    //----------------------------------------------
+    public double getY() {
+        // you will remove this line
+        return 0.0;
+    }
+    
+    //----------------------------------------------
+    // getRadius - returns the value of radius
+    //----------------------------------------------
+    public double getRadius() {
+        // you will remove this line
+        return 0.0;
+    }
+
+    //----------------------------------------------
+    // setX - assigns a new value to x
+    //----------------------------------------------
+    public void setX(double xV) {
+
+    }
+    
+
+    //----------------------------------------------
+    // setY - assigns a new value to y
+    //----------------------------------------------
+    public void setY(double yV) {
+
+    }   
+    
+    
+    //----------------------------------------------
+    // setRadius - assigns a new value to radius
+    //----------------------------------------------
+    public void setRadius(double radiusV) {
+
+    }
+    
+    //--------------------------------------------------------
+    // diameter - calculates the diameter of the circle
+    //--------------------------------------------------------
+    public double diameter() {
+        // you will remove this line
+        return 0.0;
+    }
+    
+
+    //--------------------------------------------------------
+    // area - returns the area of the circle
+    //--------------------------------------------------------
+    public double area() {
+        // you will remove this line
+        return 0.0;
+    }
+
+    //--------------------------------------------------------
+    // perimeter - returns the perimeter of the circle
+    //--------------------------------------------------------
+    public double perimeter() {
+        // you will remove this line
+        return 0.0;
+    }
+    
+    //--------------------------------------------------------
+    // isUnitCircle - return true if the radius of this circle
+    //                is 1 and its center is (0,0) and false
+    //                otherwise.
+    //--------------------------------------------------------
+    public boolean isUnitCircle() {
+        // you will remove this line
+        return true;
+    }
+
+    public boolean equals(Circle anotherCircle) {
+        // you will remove this line
+        return true;
+    }
+
+    public boolean isConcentric(Circle anotherCircle) {
+        // you will remove this line
+        return true;
+    }
+
+    public double distance(Circle anotherCircle) {
+        // you will remove this line
+        return 0.0;
+    }
+
+    public boolean intersects(Circle anotherCircle) {
+        // you will remove this line
+        return true;
+    }
+
+    // you are welcome to add more helper methods if needed
+
+}
